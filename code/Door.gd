6aac9451ca9d6b1f2c616189f875e4aee0a2b76b
Graggor <edwin@immortale.net>
2020-11-01 12:52:00 +0100

    Added a lamp. Added Stairs. Added a brick tilemap. Added basis for new player (no arm)

---
+++ code/Door.gd
@@ -0,0 +1,47 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var can_interact = false
+var is_open = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer
+	$Label.visible = false
+	set_collision_layer_bit(0, true)
+
+func _input(event):
+	if event.is_action_pressed("interact") && can_interact:
+		if is_open:
+			close()
+		else:
+			open()
+
+func _on_Area2D_body_entered(_body):
+	$Label.visible = true
+	can_interact = true
+
+func _on_Area2D_body_exited(_body):
+	$Label.visible = false
+	can_interact = false
+
+func open():
+	set_collision_layer_bit(0, false)
+	$AnimationPlayer.play("open")
+	$Label.text = "Press \"E\" to close the door."
+	is_open = true
+	#yield($AnimationPlayer, "animation_finished")
+	$LightOccluder2D.visible = false
+	
+
+func close():
+	set_collision_layer_bit(0, true)
+	$AnimationPlayer.play_backwards("open")
+	$Label.text = "Press \"E\" to open the door."
+	is_open = false
+	yield($AnimationPlayer, "animation_finished")
+	$LightOccluder2D.visible = true
+	
